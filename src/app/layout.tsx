7b9a853a761d8e940904f4daf2e692e9c8cb9a7e
Julian Nunes <contato@juliannunes.com.br>
2026-07-25 22:42:55 -0300

    feat: integrate Google Tag Manager and update Google Analytics setup for improved tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,22 +64,42 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="pt-BR" data-scroll-behavior="smooth">
       <body className={`${playfair.variable} ${manrope.variable}`}>
-        {gaId && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaId}');
-              `}
-            </Script>
-          </>
-        )}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KL5F5S4P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KL5F5S4P');
+          `}
+        </Script>
+
+        {/* Google Ads (AdWords) & Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18347937410"
+          strategy="afterInteractive"
+        />
+        <Script id="google-adwords" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18347937410');
+            ${gaId ? `gtag('config', '${gaId}');` : ""}
+          `}
+        </Script>
+
         <Navbar />
         <main>{children}</main>
         <Footer />
